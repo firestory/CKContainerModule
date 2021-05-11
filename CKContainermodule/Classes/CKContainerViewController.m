f16516e15640affdef11b67739c6abf8c8457a6a
fadil.zeng <fadil.zeng@kikatech.com>
2021-05-11 13:35:51 +0800

    init

--- CKContainermodule/Classes/CKContainerViewController.m
+++ CKContainermodule/Classes/CKContainerViewController.m
@@ -156,5 +156,19 @@
     [self.containerView addConstraint:[NSLayoutConstraint constraintWithItem:self.selectedViewControllerView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.containerView attribute:NSLayoutAttributeTrailing multiplier:1 constant:0]];
 }
 
++ (instancetype)colorFromHexString:(NSString *)hexString
+{
+    unsigned rgbValue = 0;
+    hexString = [hexString stringByReplacingOccurrencesOfString:@"#" withString:@""];
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner scanHexInt:&rgbValue];
+    
+    return [[self class] colorWithR:((rgbValue & 0xFF0000) >> 16) G:((rgbValue & 0xFF00) >> 8) B:(rgbValue & 0xFF) A:1.0];
+}
+
++ (instancetype)colorWithR:(CGFloat)red G:(CGFloat)green B:(CGFloat)blue A:(CGFloat)alpha
+{
+    return [[self class] colorWithRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:alpha];
+}
 
 @end
